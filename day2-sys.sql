8dca337011fbc8541a31b271833b0bcff4459c7a
ilya kochergin <koch@econ.msu.ru>
2018-10-09 17:04:58 +0300

    demo create spfile from memory

--- day2-sys.sql
+++ day2-sys.sql
@@ -17,3 +17,65 @@ execute    dbms_xdb_config.sethttpsport(5500)
 
 
 
+create user mydba identified by 123;
+
+grant connect to mydba;
+
+
+select * from v$pwfile_users;
+
+
+grant sysbackup to mydba; 
+
+grant sysdba to mydba; 
+
+select * from v$instance ;
+
+help shutdown
+
+alter system set sga_max_size=2G scope=spfile ;
+
+--System altered.
+
+alter system set sga_target=3G scope=spfile ;
+
+--System altered.
+
+alter system checkpoint ;
+
+--System altered.
+
+startup force
+--ORA-00823: Specified value of sga_target greater than sga_max_size
+--ORA-01078: failure in processing system parameters
+--SQL> startup nomount
+--ORA-00823: Specified value of sga_target greater than sga_max_size
+--ORA-01078: failure in processing system parameters
+create pfile='/tmp/init.ora' from spfile ;
+
+--File created.
+
+--SQL> host givm
+
+
+startup pfile='/tmp/init.ora'
+--ORACLE instance started.
+--
+--Total System Global Area 5368709120 bytes
+--Fixed Size		    8632016 bytes
+--Variable Size		 2919237936 bytes
+--Database Buffers	 2432696320 bytes
+--Redo Buffers		    8142848 bytes
+--Database mounted.
+--Database opened.
+create spfile from pfile='/tmp/init.ora' ;
+
+--File created.
+
+create spfile from memory;
+
+--File created.
+
+
+
+
