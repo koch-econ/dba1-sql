d30a5f02596311abebdf7137e110d265a06a3a30
ilya kochergin <koch@econ.msu.ru>
2018-10-09 14:46:46 +0300

    start stop

--- day2-sys.sql
+++ day2-sys.sql
@@ -4,4 +4,16 @@ SELECT * FROM v$memory_dynamic_components;
 
 show sga
 
+-- start stop database 
+
+alter system register ;
+
+begin
+    dbms_xdb_config.sethttpsport(5500);
+end ;
+/
+
+execute    dbms_xdb_config.sethttpsport(5500)
+
+
 
