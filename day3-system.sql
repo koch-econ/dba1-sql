8429352d697b901dddf2c248ac9f6a5f719ab50b
ilya kochergin <koch@econ.msu.ru>
2018-10-10 16:29:04 +0300

    tablespace demo

--- day3-system.sql
+++ day3-system.sql
@@ -3,7 +3,7 @@ SELECT
 FROM
     database_properties;
 
-SELECT
+SELECT file#,
     name
 FROM
     v$datafile;
@@ -16,6 +16,20 @@ CREATE smallfile tablespace "DEMO" datafile '/u01/app/oracle/oradata/mydb0/demo0
 
 blocksize 32 K logging default nocompress no inmemory online extent management local autoallocate segment space management
 auto;
+
+
+alter database default tablespace users; 
+
+drop tablespace demo including contents and datafiles;
+
+grant sysdba to mysys identified by 123 ;
+
+
+ CREATE TABLESPACE "DEMO" DATAFILE 
+  '/u01/app/oracle/oradata/mydb0/demo01.dbf' SIZE 52428800
+  LOGGING BLOCKSIZE 32768
+  AUTOALLOCATE DEFAULT 
+  SEGMENT SPACE MANAGEMENT AUTO;
 
 ALTER TABLESPACE demo READ ONLY;
 
@@ -34,7 +48,7 @@ TABLESPACE demo
             student1.members;
 
 select * FROM
-            student1.members;
+            student1.memb2;
  
  ALTER TABLESPACE demo OFFLINE;
  
@@ -43,7 +57,36 @@ select * FROM
             
 select * from V$datafile ;       
 
-sho user
+
+ALTER TABLESPACE demo offline normal ;
+ALTER TABLESPACE demo OnLINe;
+
+ALTER TABLESPACE demo offline immediate;
+recover tablespace demo ;
+ALTER TABLESPACE demo OnLINe;
+
+
+ALTER TABLESPACE demo offline temporary;
+
+ALTER TABLESPACE demo OnLINe;
+
+
+
+
+
+
+
+SELECT file#,
+    name
+FROM
+    v$datafile;
+
+
+alter database datafile 2 offline;
+
+alter database datafile 2 online;
+
+
 
 
  
