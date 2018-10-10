82309384cfb0b33f978e0b867e076e60fe512b62
ilya kochergin <koch@econ.msu.ru>
2018-10-10 15:09:19 +0300

    tablespace offlne normal`

--- day3-system.sql
+++ day3-system.sql
@@ -1,26 +1,50 @@
-select * from database_properties;
+SELECT
+    *
+FROM
+    database_properties;
 
-select name from v$datafile ;
+SELECT
+    name
+FROM
+    v$datafile;
 
-show parameter 32k
+SHOW parameter 32k
 
 alter system set db_32k_cache_size=5m;
 
-CREATE smallfile TABLESPACE "DEMO"
- DATAFILE
- '/u01/app/oracle/oradata/mydb0/demo01.dbf' SIZE 50M AUTOEXTEND ON NEXT 100M
- BLOCKSIZE 32K
- LOGGING
- DEFAULT NOCOMPRESS NO INMEMORY
- ONLINE
- EXTENT MANAGEMENT LOCAL AUTOALLOCATE
- SEGMENT SPACE MANAGEMENT AUTO;
+CREATE smallfile tablespace "DEMO" datafile '/u01/app/oracle/oradata/mydb0/demo01.dbf' size 50 M autoextend ON next 100 M
+
+blocksize 32 K logging default nocompress no inmemory online extent management local autoallocate segment space management
+auto;
+
+ALTER TABLESPACE demo READ ONLY;
+
+ALTER TABLESPACE demo READ WRITE;
+
+ALTER TABLESPACE demo OFFLINE;
+
+ALTER TABLESPACE demo ONLINE;
+
+CREATE TABLE student1.memb2
+TABLESPACE demo
+    AS
+        SELECT
+            *
+        FROM
+            student1.members;
+
+select * FROM
+            student1.members;
  
+ ALTER TABLESPACE demo OFFLINE;
  
- 
- alter tablespace demo read only ;
- 
- alter tablespace demo read write;
- 
+select * FROM
+            student1.memb2;
+            
+select * from V$datafile ;       
+
+sho user
+
+
  
  
