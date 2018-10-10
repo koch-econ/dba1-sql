c6d2718879d7f5ccef2f0d997a1bf56ac71c7656
ilya kochergin <koch@econ.msu.ru>
2018-10-10 15:01:57 +0300

    flashback table

--- day3-system.sql
+++ day3-system.sql
@@ -6,3 +6,21 @@ show parameter 32k
 
 alter system set db_32k_cache_size=5m;
 
+CREATE smallfile TABLESPACE "DEMO"
+ DATAFILE
+ '/u01/app/oracle/oradata/mydb0/demo01.dbf' SIZE 50M AUTOEXTEND ON NEXT 100M
+ BLOCKSIZE 32K
+ LOGGING
+ DEFAULT NOCOMPRESS NO INMEMORY
+ ONLINE
+ EXTENT MANAGEMENT LOCAL AUTOALLOCATE
+ SEGMENT SPACE MANAGEMENT AUTO;
+ 
+ 
+ 
+ alter tablespace demo read only ;
+ 
+ alter tablespace demo read write;
+ 
+ 
+ 
